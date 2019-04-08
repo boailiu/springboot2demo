b186fa6595bc5d9db77e789d6c5e22ef3114f8e3
liuboai <18810901242@163.com>
2019-04-08 19:12:52 +0800

    添加测试类;

--- src/main/java/com/boai/springboot2demo/Controller/RedisController.java
+++ src/main/java/com/boai/springboot2demo/Controller/RedisController.java
@@ -1,8 +1,11 @@
 package com.boai.springboot2demo.Controller;
 
+import com.boai.springboot2demo.Util.JSONUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 @RestController
@@ -33,12 +37,45 @@ public class RedisController {
         return map;
     }
 
+    @GetMapping("/addUser")
+    public Map<String, Object> addUser() {
+        JSONObject user = new JSONObject();
+        JSONUtil.put(user, "id", 3);
+        JSONUtil.put(user, "name", "userName1");
+        JSONUtil.put(user, "email", "user@123.com");
+        redisTemplate.opsForValue().set("userSet:user1", user.toString());
+        return new HashMap<>();
+    }
+
     @GetMapping
     @RequestMapping("/getValue/{key}")
     public Map<String, Object> getValue(@PathVariable("key") String key) {
         Object value = redisTemplate.opsForValue().get(key);
         HashMap<String, Object> map = new HashMap<>();
         map.put(key, value);
+        return map;
+    }
+
+    @GetMapping
+    @RequestMapping("/getUser")
+    public Map<String, Object> getUser() {
+        Object user = redisTemplate.opsForValue().get("userSet:user1");
+        JSONObject userObject = null;
+        HashMap<String, Object> map = new HashMap<>();
+        if (user instanceof String) {
+            try {
+                userObject = new JSONObject((String) user);
+            } catch (JSONException e) {
+                logger.error("解析user错误", e);
+            }
+        }
+        if (userObject != null) {
+            Iterator keys = userObject.keys();
+            while (keys.hasNext()) {
+                Object key = keys.next();
+                map.put(key.toString(), userObject.opt(key.toString()));
+            }
+        }
         return map;
     }
 
