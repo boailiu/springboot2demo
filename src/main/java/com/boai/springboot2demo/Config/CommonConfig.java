fcce3d3374d90ae3fb0231886c1e5a05b93e854a
liuboai <18810901242@163.com>
2019-04-30 15:35:00 +0800

    增加公共配置;修改启动banner;

---
+++ src/main/java/com/boai/springboot2demo/Config/CommonConfig.java
@@ -0,0 +1,20 @@
+package com.boai.springboot2demo.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CommonConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*");
+            }
+        };
+    }
+}
