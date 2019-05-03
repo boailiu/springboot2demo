a478111a4f97667a73e4578a4a037c5d9f32d6d9
boai <18810901242@163.com>
2019-05-03 16:28:15 +0800

    邮件demo;

--- src/main/java/com/boai/springboot2demo/Controller/MailController.java
+++ src/main/java/com/boai/springboot2demo/Controller/MailController.java
@@ -5,11 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,6 +39,21 @@ public class MailController {
         simpleMailMessage.setText(mailContent);
         javaMailSender.send(simpleMailMessage);
         logger.info("测试邮件发送成功...");
+        return map;
+    }
+
+    @GetMapping("/sendMail/html")
+    public Map<String, Object> sendMailWithHtml() throws MessagingException {
+        Map<String, Object> map = new HashMap<>();
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        helper.setFrom("18810901242@163.com");
+        helper.setTo("18810901242@163.com");
+        helper.setSubject("来自springbootdemo的测试邮件（dev）");
+        String htmlContent = "<html><body><div>hello mail</div></body></html>";
+        helper.setText(htmlContent,true);
+        javaMailSender.send(mimeMessage);
+        map.put("code", 2000);
         return map;
     }
 }
