69b91f2cebeb68f4d6a69a7c539cd80e6cfef384
liuboai <18810901242@163.com>
2020-01-02 18:07:05 +0800

    redis配置;

--- src/main/java/com/boai/springboot2demo/Config/CacheConfig.java
+++ src/main/java/com/boai/springboot2demo/Config/CacheConfig.java
@@ -7,12 +7,16 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
+import java.time.Duration;
+
 @Configuration
 public class CacheConfig {
 
     @Bean
     public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60L))
+                .computePrefixWith(cacheNames -> "springbootDemo:" + cacheNames);
         return RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(configuration)
                 .build();
     }
