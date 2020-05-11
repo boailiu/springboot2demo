aea4a98dc5f8aebb67bf30270641f4528a3fcc35
liuboai <18810901242@163.com>
2020-05-11 15:29:38 +0800

    启动类nacos注解更换;

--- src/main/java/com/boai/springboot2demo/Application.java
+++ src/main/java/com/boai/springboot2demo/Application.java
@@ -1,6 +1,6 @@
 package com.boai.springboot2demo;
 
-import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +9,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @MapperScan("com.boai.springboot2demo.Mapper")
 @EnableScheduling
-@NacosConfigurationProperties(dataId = "example",autoRefreshed = true)
+@NacosPropertySource(dataId = "example",autoRefreshed = true)
+//@NacosConfigurationProperties(dataId = "springboot2demo",groupId = "demo",autoRefreshed = true)
 public class Application {
 
     public static void main(String[] args) {
